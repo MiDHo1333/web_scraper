25e86ed3c989aca0bdba726365df3b19957c0e0a
Beverly Abadines <BeverlyAb@users.noreply.github.com>
2021-07-16 21:39:16 -0700

    fixed bug. id_list was reseted in inner for loop

--- link_maker.py
+++ link_maker.py
@@ -1,7 +1,10 @@
 '''linked_maker.py
 gets list of ids from initial url
-To Do: Create valid links to sift through job requirements. 
-Example: https://www.linkedin.com/jobs/view/data-scientist-at-cvs-health-2652878200?refId=YDF7%2BPAzajYP39710xFV3Q%3D%3D&trackingId=H%2FkZRfHnlk%2BXQsFxJAQJxw%3D%3D&position=17&pageNum=0&trk=public_jobs_jserp-result_search-card
+To Do: Create valid links to sift through job requirements.
+Example: 
+From : 2652878200 
+To: https://www.linkedin.com/jobs/view/2636853058/?alternateChannel=search&refId=V7qh%2F7YOXM6rnRdfIu8lCA%3D%3D&trackingId=YCM8qAXxtQH80KA9b2C6hA%3D%3D
+
 '''
 from bs4 import BeautifulSoup as bs
 import re as re
@@ -11,16 +14,20 @@ import requests
 
 max_positions = 23 #positions per page, limited by LinkedIn
 max_pages = 10
+id_list = []
+
 for i in range(max_positions):
     for j in range(max_pages): 
         url = f"https://www.linkedin.com/jobs/search/?currentJobId=2557487241&keywords=data%20scientist&position={i}&pageNum={j}"
         res = requests.get(url)
         soup = bs(res.text,'html.parser')
         job_container = soup.findAll("a",class_="base-card__full-link")
-        id_list = []
+        
         for job in job_container:
             job = re.findall(r'\d+',str(job))
+
             id_list.append(job[0]+",")
+
 
 print(len(id_list))
 df = pd.DataFrame(id_list)
