e90deec928ec16babe31d0ea75b146b0e16f2524
Beverly Abadines <BeverlyAb@users.noreply.github.com>
2021-07-16 20:05:20 -0700

    added job_id lists and link_maker.py
    link_maker is 90% done

---
+++ link_maker.py
@@ -0,0 +1,23 @@
+'''linked_maker.py
+gets list of ids from initial url
+To Do: Create valid links to sift through job requirements. 
+Example: https://www.linkedin.com/jobs/view/data-scientist-at-cvs-health-2652878200?refId=YDF7%2BPAzajYP39710xFV3Q%3D%3D&trackingId=H%2FkZRfHnlk%2BXQsFxJAQJxw%3D%3D&position=17&pageNum=0&trk=public_jobs_jserp-result_search-card
+'''
+from bs4 import BeautifulSoup as bs
+import re as re
+import pandas as pd
+import os
+import requests
+
+
+url = f"https://www.linkedin.com/jobs/search/?currentJobId=2557487241&keywords=data%20scientist&position=1&pageNum=0"
+soup = bs(res.text,'html.parser')
+job_container = soup.findAll("a",class_="base-card__full-link")
+id_list = []
+for job in job_container:
+    job = re.findall(r'\d+',str(job))
+    id_list.append(job[0])
+
+print(len(id_list))
+df = pd.DataFrame(id_list)
+df.to_csv("job_idszzz.csv", encoding='utf-8', index=False,header=False,mode='a')
