d86dce34824d31d6877d697ae861034d7e343130
Beverly Abadines <BeverlyAb@users.noreply.github.com>
2021-07-16 20:11:18 -0700

    typos

--- link_maker.py
+++ link_maker.py
@@ -9,15 +9,17 @@ import pandas as pd
 import os
 import requests
 
-
-url = f"https://www.linkedin.com/jobs/search/?currentJobId=2557487241&keywords=data%20scientist&position=1&pageNum=0"
-soup = bs(res.text,'html.parser')
-job_container = soup.findAll("a",class_="base-card__full-link")
-id_list = []
-for job in job_container:
-    job = re.findall(r'\d+',str(job))
-    id_list.append(job[0])
+for i in range(20):
+    url = f"https://www.linkedin.com/jobs/search/?currentJobId=2557487241&keywords=data%20scientist&position=1&pageNum={i}"
+    # url = "https://www.linkedin.com/jobs/search/?currentJobId=2557487241&keywords=data%20scientist"
+    res = requests.get(url)
+    soup = bs(res.text,'html.parser')
+    job_container = soup.findAll("a",class_="base-card__full-link")
+    id_list = []
+    for job in job_container:
+        job = re.findall(r'\d+',str(job))
+        id_list.append(job[0])
 
 print(len(id_list))
 df = pd.DataFrame(id_list)
-df.to_csv("job_idszzz.csv", encoding='utf-8', index=False,header=False,mode='a')
+df.to_csv("job_id.csv", encoding='utf-8', index=False,header=False,mode='a')
