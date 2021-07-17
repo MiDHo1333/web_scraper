bcf995375c1c0a45ab4de8158d842d92df4602d3
Beverly Abadines <BeverlyAb@users.noreply.github.com>
2021-07-17 08:19:33 -0700

    graphs

--- link_maker.py
+++ link_maker.py
@@ -1,6 +1,6 @@
 '''linked_maker.py
-gets list of ids from initial url
-To Do: Create valid links to sift through job requirements.
+ gets list of ids from initial url and converts them into scrapable links with job descriptions
+
 Example: 
 From : 2652878200 
 To: https://www.linkedin.com/jobs/view/2636853058/?alternateChannel=search&refId=V7qh%2F7YOXM6rnRdfIu8lCA%3D%3D&trackingId=YCM8qAXxtQH80KA9b2C6hA%3D%3D
@@ -14,21 +14,22 @@ import requests
 
 max_positions = 23 #positions per page, limited by LinkedIn
 max_pages = 10
-id_list = []
+links = []
+
 
 for i in range(max_pages):
     for j in range(max_positions): 
-        url = f"https://www.linkedin.com/jobs/search/?currentJobId=2557487241&keywords=data%20scientist&position={i}&pageNum={j}"
+        url = f"https://www.linkedin.com/jobs/search/?currentJobId=2557487241&keywords=data%20scientist&position={j}&pageNum={i}"
         res = requests.get(url)
         soup = bs(res.text,'html.parser')
         job_container = soup.findAll("a",class_="base-card__full-link")
          
         for job in job_container:
             job = re.findall(r'\d+',str(job))
+            link =  f"https://www.linkedin.com/jobs/view/{job[0]}/?alternateChannel=search&refId=V7qh%2F7YOXM6rnRdfIu8lCA%3D%3D&trackingId=YCM8qAXxtQH80KA9b2C6hA%3D%3D"
+            links.append(link+",")
+            print(link+",")
 
-            id_list.append(job[0]+",")
-
-
-print(len(id_list))
-df = pd.DataFrame(id_list)
+print(len(links))
+df = pd.DataFrame(links)
 df.to_csv("job_id.csv", encoding='utf-8', index=False,header=False,mode='a')
